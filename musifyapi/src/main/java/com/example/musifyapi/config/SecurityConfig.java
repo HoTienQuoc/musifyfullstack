ac965863f1c4fef4d585f1121aa30e2019b9de2d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-10-09 13:00:25 +0700

    Enhance CORS configuration in SecurityConfig by adding a CorsFilter bean and updating allowed origins to use patterns, improving security and flexibility for API requests.

--- musifyapi/src/main/java/com/example/musifyapi/config/SecurityConfig.java
+++ musifyapi/src/main/java/com/example/musifyapi/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -33,9 +34,15 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsFilter corsFilter(){
+        return new CorsFilter(corsConfigurationSource());
+    }
+
+    @Bean
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.addAllowedOriginPattern("*");
+        // configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList(
             "GET","POST","PUT","DELETE","OPTIONS","PATCH"
         ));
@@ -43,6 +50,7 @@ public class SecurityConfig {
             "Authorization", "Content-Type"
         ));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
